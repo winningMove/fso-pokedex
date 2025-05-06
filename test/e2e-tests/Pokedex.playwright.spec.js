decd1ffae668facfa68f42a9a95270b519e201f5
winningMove <dariopapic+github@gmail.com>
2025-05-06 14:45:28 +0200

    add second test

--- test/e2e-tests/Pokedex.playwright.spec.js
+++ test/e2e-tests/Pokedex.playwright.spec.js
@@ -1,9 +1,13 @@
-const { test, describe, expect } = require('@playwright/test')
+const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Pokedex', () => {
+  beforeEach(async ({ page }) => await page.goto('/'))
   test('front page can be opened', async ({ page }) => {
-    await page.goto('/')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+  test('can navigate to specific Pokemon page', async ({ page }) => {
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 })
